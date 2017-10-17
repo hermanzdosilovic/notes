7c847414e2fea8743cf35168cf614aec129b2ac7
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2017-10-17 19:32:49 +0200

    Added download PDF button

--- js/edit.js
+++ js/edit.js
@@ -33,6 +33,7 @@ $(document).ready(function() {
   } else {
     editor.setValue(Storage.get("note") || "");
     $("#shareBtn").attr("disabled", true);
+    $("#downloadPdfBtn").attr("disabled", true);
   }
 
   renderPreview(editor.getValue(), $("#preview")[0]);
@@ -42,21 +43,6 @@ function toggleVim() {
   var keyMap = document.getElementById("vimCheckbox").checked ? "vim" : "default";
   Storage.set("keyMap", keyMap);
   editor.setOption("keyMap", keyMap);
-}
-
-function saveNote(callback = function(){}) {
-  var saveBtn = $("#saveBtn")[0];
-  saveBtn.classList.add("loading"); 
-
-  Cloud.save(editor.getValue(), function(id) {
-      saveBtn.classList.remove("loading");
-      $("#shareBtn").attr("disabled", false);
-      window.history.replaceState(null, null, location.origin + location.pathname + "?" + id);
-      callback();
-    }, function() {
-      saveBtn.classList.remove("loading");
-      showError("Error", "Cannot save your note!");
-  });
 }
 
 function loadNote() {
@@ -69,10 +55,35 @@ function loadNote() {
   });
 }
 
+function saveNote(callback = function(){}) {
+  var saveBtn = $("#saveBtn")[0];
+  saveBtn.classList.add("loading"); 
+
+  Cloud.save(editor.getValue(), function(id) {
+      saveBtn.classList.remove("loading");
+      $("#shareBtn").attr("disabled", false);
+      $("#downloadPdfBtn").attr("disabled", false);
+      window.history.replaceState(null, null, location.origin + location.pathname + "?" + id);
+      callback();
+    }, function() {
+      saveBtn.classList.remove("loading");
+      showError("Error", "Cannot save your note!");
+  });
+}
+
 function downloadNote() {
   download(editor.getValue(), "note.md", "text/plain");
 }
 
+function downloadPdf() {
+  var url = "https://url-to-pdf-api.herokuapp.com/api/render?emulateScreenMedia=true&pdf.margin.top=2cm&pdf.margin.right=2cm&pdf.margin.bottom=2cm&pdf.margin.left=2cm";
+  window.open(url + "&url=" + getShareUrl(), "_blank");
+}
+
 function shareNote() {
-  window.open(location.href.substr(0, location.href.lastIndexOf("/") + 1) + "view.html?" + getIdFromURI(), "_blank");
+  window.open(getShareUrl(), "_blank");
+}
+
+function getShareUrl() {
+  return location.href.substr(0, location.href.lastIndexOf("/") + 1) + "view.html?" + getIdFromURI();
 }
